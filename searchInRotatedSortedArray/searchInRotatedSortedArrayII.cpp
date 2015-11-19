862bf70d5af3b4f52f42849cfe447d7f753bd9da
weekend27 <weekend27@163.com>
2015-11-19 17:04:11 +0800

    searchInRotatedSortedArray

--- searchInRotatedSortedArray/searchInRotatedSortedArrayII.cpp
+++ searchInRotatedSortedArray/searchInRotatedSortedArrayII.cpp
@@ -1,3 +1,19 @@
+// Source : https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+// Author : weekend27
+// Date   : 2015-11-19
+
+/********************************************************************************** 
+
+Follow up for "Search in Rotated Sorted Array":
+
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+Write a function to determine if a given target is in the array.
+         
+**********************************************************************************/
+
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
