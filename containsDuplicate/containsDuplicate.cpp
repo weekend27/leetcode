4242bca1a904a75da47eb1e0481bf519ef17ddfd
weekend27 <weekend27@163.com>
2015-12-03 01:01:04 +0800

    containsDuplicate

---
+++ containsDuplicate/containsDuplicate.cpp
@@ -0,0 +1,24 @@
+// Source : https://leetcode.com/problems/contains-duplicate/
+// Author : weekend27
+// Date   : 2015-11-27
+
+/********************************************************************************** 
+
+Given an array of integers, find if the array contains any duplicates. 
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+          
+**********************************************************************************/
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if (nums.size() <= 1) return false;
+        vector<int>& array = nums;
+        std::sort(array.begin(),array.end());
+        for(int i = 1; i < array.size(); i++){
+            if (array[i] == array[i-1])  return true;
+        }
+        return false;
+    }
+};
