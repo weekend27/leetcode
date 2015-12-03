7edf72738081ca7d7d3359309c5534cd335f47d4
weekend27 <weekend27@163.com>
2015-12-03 08:47:43 +0800

    removeDuplicatesFromSortedArray

--- removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.cpp
+++ removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.cpp
@@ -2,7 +2,7 @@
 // Author : weekend27
 // Date   : 2015-11-18
 
-/********************************************************************************** 
+/**********************************************************************************
 
 Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
 
@@ -13,18 +13,22 @@ For example,
 Given input array nums = [1,1,2],
 
 Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
-           
+
 **********************************************************************************/
+
+// How to do it:
+// Two pointers: when one index meets duplicates, another one will stay and not increase. 
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int index = 0;
         int len = nums.size();
-        
+
         if (len == 0){
             return 0;
         }
-        
+
         for (int i = 1; i < len; i++){
             if (nums[index] != nums[i])
                 nums[++index] = nums[i];
--- removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArrayII.cpp
+++ removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArrayII.cpp
@@ -2,7 +2,7 @@
 // Author : weekend27
 // Date   : 2015-11-18
 
-/********************************************************************************** 
+/**********************************************************************************
 
 Follow up for "Remove Duplicates":
 What if duplicates are allowed at most twice?
@@ -11,21 +11,25 @@ For example,
 Given sorted array nums = [1,1,1,2,2,3],
 
 Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
-          
+
 **********************************************************************************/
+
+// How to do it:
+// Two pointers: add one variable to record the times that elements appear.
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int len = nums.size();
         int index = 2;
-        
+
         if (len <= 2) return len;
-        
+
         for (int i = 2; i < len; i++){
             if (nums[index-2] != nums[i])
                 nums[index++] = nums[i];
         }
-        
+
         return index;
     }
 };
