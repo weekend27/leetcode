3511596df6d75e8079ddd5e95e3ef6515a8e5394
weekend27 <weekend27@163.com>
2015-12-03 08:28:20 +0800

    containsDuplicate

--- containsDuplicate/containsDuplicate.cpp
+++ containsDuplicate/containsDuplicate.cpp
@@ -2,13 +2,15 @@
 // Author : weekend27
 // Date   : 2015-11-27
 
-/********************************************************************************** 
+/**********************************************************************************
 
-Given an array of integers, find if the array contains any duplicates. 
+Given an array of integers, find if the array contains any duplicates.
 Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
-          
+
 **********************************************************************************/
 
+// How to do it:
+// two steps: sort the vector first, and then check if the adjacent elements are the same
 
 class Solution {
 public:
