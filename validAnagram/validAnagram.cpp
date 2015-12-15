5c687e254ac8e651fcc194717b07451585a806df
weekend27 <weekend27@163.com>
2015-12-15 19:37:40 +0800

    validAnagram.cpp

---
+++ validAnagram/validAnagram.cpp
@@ -0,0 +1,31 @@
+// Source : https://leetcode.com/problems/valid-anagram/
+// Author : weekend27
+// Date   : 2015-12-15
+
+/**********************************************************************************
+
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+
+**********************************************************************************/
+
+// How to do it:
+// first sort, then check if they are equal
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
