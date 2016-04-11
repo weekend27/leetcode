b858cbb3d3634c64ec5ef8462a6edf7a3c83d16c
weekend27 <weekend27@163.com>
2016-04-11 20:11:03 +0800

    addTwoNumbers

---
+++ addTwoNumbers/addTwoNumbers.java
@@ -0,0 +1,50 @@
+// Source : https://leetcode.com/problems/add-two-numbers/
+// Author : weekend27
+// Date   : 2015-04-11
+
+/**********************************************************************************
+
+You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+
+**********************************************************************************/
+
+// How to do it:
+// ref: https://leetcode.com/discuss/2308/is-this-algorithm-optimal-or-what
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        ListNode sentinel = new ListNode(0);
+        ListNode d = sentinel;
+        int sum = 0;
+        while (c1 != null || c2 != null) {
+            sum /= 10;
+            if (c1 != null) {
+                sum += c1.val;
+                c1 = c1.next;
+            }
+            if (c2 != null) {
+                sum += c2.val;
+                c2 = c2.next;
+            }
+            d.next = new ListNode(sum % 10);
+            d = d.next;
+        }
+        if (sum / 10 == 1) {
+            d.next = new ListNode(1);
+        }
+        return sentinel.next;
+    }
+}
