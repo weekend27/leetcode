c718053c837054eb76a77156f191c3df77b82f83
weekend27 <weekend27@163.com>
2016-04-12 21:49:30 +0800

    regexMatching

--- regularExpressionMatching/regularExpressionMatching.java
+++ regularExpressionMatching/regularExpressionMatching.java
@@ -28,23 +28,13 @@ isMatch("aab", "c*a*b") → true
 // How to do it:
 // ref: http://www.programcreek.com/2012/12/leetcode-regular-expression-matching-in-java/
 /*
-First of all, this is one of the most difficulty problems. It is hard to think through all different cases. The problem should be simplified to handle 2 basic cases:
+First of all, this is one of the most difficult problems. It is hard to think through all different cases. The problem should be simplified to handle 2 basic cases:
 the second char of pattern is "*"
 the second char of pattern is not "*"
 For the 1st case, if the first char of pattern is not ".", the first char of pattern and string should be the same. Then continue to match the remaining part.
 
 For the 2nd case, if the first char of pattern is "." or first char of pattern == the first i char of string, continue to match the remaining part.
 */
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-
 
 public class regularExpressionMatching {
     public boolean isMatch(String s, String p) {
